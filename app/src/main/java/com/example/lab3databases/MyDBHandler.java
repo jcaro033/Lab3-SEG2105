908c18b7718b7031259c08917f982ab16f25f1d3
jcaro033 <jcaro033@uottawa.ca>
2025-10-20 22:19:10 -0400

    Init commit and DB handler

---
+++ app/src/main/java/com/example/lab3databases/MyDBHandler.java
@@ -0,0 +1,82 @@
+package com.example.lab3databases;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+    private static final String TABLE_NAME = "products";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_PRODUCT_NAME = "name";
+    private static final String COLUMN_PRODUCT_PRICE = "price";
+    private static final String DATABASE_NAME = "products.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyDBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create_table_cmd = "CREATE TABLE " + TABLE_NAME +
+                "(" + COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                COLUMN_PRODUCT_NAME + " TEXT, " +
+                COLUMN_PRODUCT_PRICE + " DOUBLE " + ")";
+
+        db.execSQL(create_table_cmd);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public Cursor getData() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        return db.rawQuery(query, null); // returns "cursor" all products from the table
+    }
+
+    public void addProduct(Product product) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_PRODUCT_NAME, product.getProductName());
+        values.put(COLUMN_PRODUCT_PRICE, product.getProductPrice());
+
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+    public Cursor findProduct(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_PRODUCT_NAME + " = '" + name +"'";
+        return db.rawQuery(query,null);
+    }
+    public Cursor findProduct(double price) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_PRODUCT_PRICE + " = '" + price +"'";
+        return db.rawQuery(query,null);
+    }
+
+    public Cursor findProduct(String name, double price) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_PRODUCT_PRICE + " = '" + price +"' AND " + " WHERE " + COLUMN_PRODUCT_NAME + " = '" + name +"'" ;
+        return db.rawQuery(query,null);
+    }
+
+    public void deleteProduct(String Id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE " + COLUMN_ID + " = '" + Id +"'";
+
+        db.execSQL(query);
+    }
+
+}
